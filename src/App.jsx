cd09cf53c5eea56cb4c7fabbac623df7d890dfaf
vlad <vladislavherasymenko@gmail.com>
2021-01-13 04:55:12 +0200

    add react app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {ReactQueryDevtools} from 'react-query-devtools';
+import Navbar from './components/Navbar';
+import Planets from './components/Planets';
+import People from './components/People';
+
+
+const App = () => {
+    const [page, setPage] = useState('planets');
+    return (
+        <>
+            <div className="App">
+                <h1>Star Wars Info</h1>
+                <Navbar setPage={setPage}/>
+                <div className="content">
+                    {page === 'planets'
+                        ? <Planets/>
+                        : <People/>
+                    }
+                </div>
+            </div>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </>
+    );
+}
+
+export default App;
